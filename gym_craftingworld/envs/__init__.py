54a6c3a95502d4dc0aadaa12b8ead478ba8123c0
Declan Millar <declan.millar@ibm.com>
2021-04-09 20:50:12 +0100

    Lint env/init

--- gym_craftingworld/envs/__init__.py
+++ gym_craftingworld/envs/__init__.py
@@ -1,2 +1,4 @@
+"""Import classes from submodules"""
+
 from gym_craftingworld.envs.craftingworld_env import CraftingWorldEnv
 from gym_craftingworld.envs.craftingworld_env_rgb import CraftingWorldEnvRGB

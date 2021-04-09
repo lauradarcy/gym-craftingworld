6b713711e6bbf6c1ea5172bf11f525f82c99fafb
Declan Millar <declan.millar@ibm.com>
2021-04-09 20:42:31 +0100

    linting

--- gym_craftingworld/__init__.py
+++ gym_craftingworld/__init__.py
@@ -1,10 +1,14 @@
+"""Register gym environments"""
+
 from gym.envs.registration import register
 
-register(id='craftingworld-v0',
-         entry_point='gym_craftingworld.envs:CraftingWorldEnv',
-         )
+register(
+    id='craftingworld-v0',
+    entry_point='gym_craftingworld.envs:CraftingWorldEnv',
+)
 
-register(id='craftingworld-v2',
-         entry_point='gym_craftingworld.envs:CraftingWorldEnv',
-         kwargs={'size': (8,8)}
-         )
+register(
+    id='craftingworld-v2',
+    entry_point='gym_craftingworld.envs:CraftingWorldEnv',
+    kwargs={'size': (8, 8)},
+)
